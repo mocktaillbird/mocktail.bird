a5058fd3d13d04bbcfe94d51608e548cd05ed92a
Ravi Sankar Karuturi <ravisankarkaruturi@gmail.com>
2019-03-06 00:20:22 -0800

    FEATURE:Implemented basic config from application properties

--- service/src/main/java/bird/mocktail/me/Application.java
+++ service/src/main/java/bird/mocktail/me/Application.java
@@ -1,14 +1,20 @@
-package bird.mocktail.me.service.application;
+package bird.mocktail.me;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+	
+
+	private static final Logger logger = LoggerFactory.getLogger(Application.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-		
+		logger.info("Starting the MockTile Bird.....");
 	}
 
 }
